86432a7941b712e2fb531261cf0fd5038699f018
connor <connorriley@outlook.com>
2023-10-16 18:42:51 -0400

    work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:8000/register', {name, email, password})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='App'>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+          <input 
+            type='text'
+            placeholder='name'
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <input 
+            type='email'
+            placeholder='email'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input 
+            type='password'
+            placeholder='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button type='submit' className='btn'>
+            Register
+          </button>
+          <br />
+          <button type='submit' className='btn'>
+            Login
+          </button>
+      </form>
+
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
